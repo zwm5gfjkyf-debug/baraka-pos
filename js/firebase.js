fe8659fc9840f2710cea33e306f4b35f2f77deb1
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-03 05:29:37 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
