92f47bf250328fe3bac1f66762c25b4071d1bcc2
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-05 19:08:38 +0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,37 @@
 
+/* =========================================
+   UTILS
+========================================= */
+
+function formatMoney(num) {
+  return Number(num || 0)
+    .toLocaleString("ru-RU")
+    .replace(/,/g, " ");
+}
+
+function safeGetUserId() {
+  return auth.currentUser ? auth.currentUser.uid : null;
+}
+
+function getStartOfToday() {
+  const d = new Date();
+  d.setHours(0,0,0,0);
+  return d;
+}
+
+function getStartOfWeek() {
+  const d = new Date();
+  const day = d.getDay() || 7;
+  if(day !== 1) {
+    d.setDate(d.getDate() - (day - 1));
+  }
+  d.setHours(0,0,0,0);
+  return d;
+}
+
+function getStartOfMonth() {
+  const d = new Date();
+  d.setDate(1);
+  d.setHours(0,0,0,0);
+  return d;
+}
