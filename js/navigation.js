3d4475a01314c047a048ccf7c9891e569ba395aa
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-05 19:09:16 +0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,35 @@
+/* =========================================
+   PAGE NAVIGATION
+========================================= */
 
+function navigate(pageId){
+
+  document.querySelectorAll(".page")
+  .forEach(p => p.classList.add("hidden"));
+
+  const page = document.getElementById(pageId);
+
+  if(page){
+    page.classList.remove("hidden");
+  }
+
+  document.querySelectorAll(".bottom-nav button")
+  .forEach(btn => btn.classList.remove("active"));
+
+  document.querySelectorAll(".bottom-nav button")
+  .forEach(btn=>{
+    if(btn.getAttribute("onclick")?.includes(pageId)){
+      btn.classList.add("active");
+    }
+  });
+
+  if(pageId === "dashboardPage") loadDashboard();
+
+  if(pageId === "salePage") loadSaleProducts();
+
+  if(pageId === "debtPage") loadDebtCustomers();
+
+  if(pageId === "stockPage") loadCurrentStock();
+
+  if(pageId === "analyticsPage") loadAnalytics();
+}
