29cdb795325e8f15d6c6cd94186c49bbc545d39b
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-05 19:08:55 +0500

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,25 @@
 
+/* =========================================
+   UI FUNCTIONS
+========================================= */
+
+function toggleProfileMenu(){
+  const menu = document.getElementById("profileMenu");
+  if(menu) menu.classList.toggle("hidden");
+}
+
+function showSuccess(message){
+
+  const overlay = document.getElementById("successOverlay");
+  const text = document.getElementById("successText");
+
+  if(!overlay || !text) return;
+
+  text.innerText = message;
+
+  overlay.classList.remove("hidden");
+
+  setTimeout(()=>{
+    overlay.classList.add("hidden");
+  },1500);
+}
