e2f1375111bd16fa1561078bce1a989f45686a6c
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-07 05:45:47 +0500

    Update sales.js

--- js/sales.js
+++ js/sales.js
@@ -110,7 +110,7 @@ function addToCart(product){
     if(existing){
 
         if(existing.qty + 1 > product.stock){
-            alert("Zaxirada yetarli mahsulot yo'q")
+           showConfirm("Zaxirada yetarli mahsulot yo'q", ()=>{})
             return
         }
 
