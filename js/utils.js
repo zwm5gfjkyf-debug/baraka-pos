11a1a180c0c3b8870be44dea28cf66e63b874f5f
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-07 10:26:59 +0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,104 +1,65 @@
+
 /* =========================================
    UTILS
 ========================================= */
 
-/* ===============================
-FORMAT MONEY
-=============================== */
-
-function formatMoney(num){
-
-return Number(num || 0)
-.toLocaleString("ru-RU")
-.replace(/,/g," ")
-
+function formatMoney(num) {
+  return Number(num || 0)
+    .toLocaleString("ru-RU")
+    .replace(/,/g, " ");
 }
-
-/* ===============================
-ANIMATED NUMBER
-=============================== */
-
 function animateNumber(el,value,duration=800){
 
-let start = 0
+let start=0
 
-const step = Math.max(1, Math.ceil(value / 80))
-const interval = setInterval(()=>{
+const step=Math.ceil(value/60)
 
-start += step
+const interval=setInterval(()=>{
 
-if(start >= value){
+start+=step
 
-start = value
-
+if(start>=value){
+start=value
 clearInterval(interval)
+}
+
+el.innerText=formatMoney(start)
+
+},duration/60)
+
+}
+function formatMoney(num){
+
+    return Number(num)
+        .toLocaleString("ru-RU")
+        .replace(/,/g," ")
 
 }
 
-el.innerText = formatMoney(start)
-
-}, duration / 60)
-
+function safeGetUserId() {
+  return auth.currentUser ? auth.currentUser.uid : null;
 }
 
-/* ===============================
-SAFE USER ID
-=============================== */
-
-function safeGetUserId(){
-
-return auth.currentUser ? auth.currentUser.uid : null
-
+function getStartOfToday() {
+  const d = new Date();
+  d.setHours(0,0,0,0);
+  return d;
 }
 
-/* ===============================
-START OF TODAY
-=============================== */
-
-function getStartOfToday(){
-
-const d = new Date()
-
-d.setHours(0,0,0,0)
-
-return d
-
+function getStartOfWeek() {
+  const d = new Date();
+  const day = d.getDay() || 7;
+  if(day !== 1) {
+    d.setDate(d.getDate() - (day - 1));
+  }
+  d.setHours(0,0,0,0);
+  return d;
 }
 
-/* ===============================
-START OF WEEK
-=============================== */
-
-function getStartOfWeek(){
-
-const d = new Date()
-
-const day = d.getDay() || 7
-
-if(day !== 1){
-
-d.setDate(d.getDate() - (day - 1))
-
+function getStartOfMonth() {
+  const d = new Date();
+  d.setDate(1);
+  d.setHours(0,0,0,0);
+  return d;
 }
-
-d.setHours(0,0,0,0)
-
-return d
-
-}
-
-/* ===============================
-START OF MONTH
-=============================== */
-
-function getStartOfMonth(){
-
-const d = new Date()
-
-d.setDate(1)
-
-d.setHours(0,0,0,0)
-
-return d
-
-}
+Is this correct for util.js
