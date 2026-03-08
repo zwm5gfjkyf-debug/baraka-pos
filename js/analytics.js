3eac2957774628477a1f70ca4d76ed441faa62d3
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-08 10:19:28 +0500

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -714,3 +714,25 @@ font:{size:10}
 
 })
 }
+function filterDebtAnalytics(){
+
+const search = document
+.getElementById("debtAnalyticsSearch")
+.value
+.toLowerCase()
+
+const cards = document.querySelectorAll("#debtAnalyticsList .dashboard-card")
+
+cards.forEach(card=>{
+
+const name = card.querySelector("h3").innerText.toLowerCase()
+
+if(name.includes(search)){
+card.style.display = ""
+}else{
+card.style.display = "none"
+}
+
+})
+
+}
