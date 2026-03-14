4b9f7c165e124efbd9e86d6c4f2b817298a7f4eb
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-14 11:05:26 +0500

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -116,3 +116,16 @@ const savedTheme = localStorage.getItem("theme")
 if(savedTheme === "light"){
 document.body.classList.add("light-mode")
 }
+function toggleTheme(){
+
+document.body.classList.toggle("light-mode")
+
+if(document.body.classList.contains("light-mode")){
+localStorage.setItem("theme","light")
+}else{
+localStorage.setItem("theme","dark")
+}
+
+updateChartsTheme()
+
+}
