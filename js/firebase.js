b40715a3ac11e4e9ee7200a9404b1c3c32cbe044
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-25 17:55:58 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -6,11 +6,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyBzs6n66fLSWBhobX-GOnROx-QvR8eH9gU",
   authDomain: "baraka-pos-2.firebaseapp.com",
   projectId: "baraka-pos-2",
-  storageBucket: "baraka-pos-2.appspot.com", // ✅ FIXED
+  storageBucket: "baraka-pos-2.firebasestorage.app",
   messagingSenderId: "3915833554",
-  appId: "1:3915833554:web:36144e4699aaf4249e0d0b"
+  appId: "1:3915833554:web:36144e4699aaf4249e0d0b",
+  measurementId: "G-0MQEE5DWLK"
 };
-
 const storage = firebase.storage(firebase.app()) // ✅ FIXED
 const storageRef = storage.ref() // ✅ ADDED
 
