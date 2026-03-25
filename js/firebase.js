0e62cbd33c68b0df4785e7c03072db46acc042eb
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-25 14:56:12 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -16,7 +16,7 @@ firebase.initializeApp(firebaseConfig);
 }
 const auth = firebase.auth();
 const db = firebase.firestore();
-
+const storage = firebase.storage()
 db.settings({
 experimentalAutoDetectLongPolling: true,
 merge: true
