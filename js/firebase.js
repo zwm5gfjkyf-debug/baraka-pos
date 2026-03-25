e0d9982e71d2e92e1460c8c5d4201b8e30a53ea6
zwm5gfjkyf-debug <zwm5gfjkyf@privaterelay.appleid.com>
2026-03-25 17:52:59 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -6,20 +6,16 @@ const firebaseConfig = {
   apiKey: "AIzaSyBzs6n66fLSWBhobX-GOnROx-QvR8eH9gU",
   authDomain: "baraka-pos-2.firebaseapp.com",
   projectId: "baraka-pos-2",
-  storageBucket: "baraka-pos-2.firebasestorage.app",
+  storageBucket: "baraka-pos-2.appspot.com", // ✅ FIXED
   messagingSenderId: "3915833554",
   appId: "1:3915833554:web:36144e4699aaf4249e0d0b"
 };
 
-if (!firebase.apps.length) {
-firebase.initializeApp(firebaseConfig);
-}
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage()
+const storage = firebase.storage(firebase.app()) // ✅ FIXED
+const storageRef = storage.ref() // ✅ ADDED
+
 db.settings({
-experimentalAutoDetectLongPolling: true,
-merge: true
+  experimentalAutoDetectLongPolling: true // ✅ FIXED
 })
 
 db.enablePersistence()
